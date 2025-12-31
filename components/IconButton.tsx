74cc219cdd01bbc47623f0a0d8655da1976b5348
Gianluca Bresolin <gianbreso02@gmail.com>
2025-12-31 18:53:04 +0100

    Add style and size props to IconButton component.

--- components/IconButton.tsx
+++ components/IconButton.tsx
@@ -1,19 +1,31 @@
 import { Ionicons } from "@expo/vector-icons";
 import { Text, TouchableOpacity } from "react-native";
+import { Styles } from "../constants/theme";
 
 export type IconButtonProps = {
     title?: string;
     onPress: () => void;
     iconName: keyof typeof Ionicons.glyphMap;
+    styleSheet?: object;
+    size?: number;
 }
 
 export default function IconButton({ props } : { props : IconButtonProps }) {
     return (
         <TouchableOpacity
             onPress={props.onPress}
+            style={props.styleSheet ? props.styleSheet : Styles.button}
         >
-            <Ionicons name={props.iconName} size = {24} />
-            {props.title && <Text>{props.title}</Text>}
+            <Ionicons 
+                name={props.iconName} 
+                size = {props.size ? props.size : 24}
+                color = {Styles.buttonText.color}
+            />
+            {props.title && <Text
+                style = {Styles.buttonText}
+            >
+                {props.title}
+            </Text>}
         </TouchableOpacity>
     )
 }
