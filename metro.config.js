586a92fb7fbc64736b6e074b618f0ac64321c97b
Gianluca Bresolin <gianbreso02@gmail.com>
2026-02-18 22:30:02 +0100

    Remove index.js, modified metro.config to directly use the expo-router entry point.

--- metro.config.js
+++ metro.config.js
@@ -2,4 +2,15 @@ const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
+// Override resolver to use expo-router/entry as main entry point
+config.resolver = {
+  ...config.resolver,
+  resolveRequest: (context, moduleName, platform) => {
+    if (moduleName === './index' || moduleName === 'index') {
+      return context.resolveRequest(context, 'expo-router/entry', platform);
+    }
+    return context.resolveRequest(context, moduleName, platform);
+  },
+};
+
 module.exports = config;
