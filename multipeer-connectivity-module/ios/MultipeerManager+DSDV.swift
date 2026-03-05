1da6e726787d2fc06bb85de8b8669d7b623ae2d5
Gianluca Bresolin <gianbreso02@gmail.com>
2026-03-05 15:25:15 +0100

    Refactor Network -> Link Delegate.

--- multipeer-connectivity-module/ios/MultipeerManager+DSDV.swift
+++ multipeer-connectivity-module/ios/MultipeerManager+DSDV.swift
@@ -1,32 +1,63 @@
 import Foundation
 
-extension MultipeerManager : DSDVManagerDelegate {
-    func broadcastUpdate(
-        destination: NodeID,
-        hops: Int,
-        sequenceNumber: Int
-    ) {
-        let payload = "0|\(destination)|\(hops)|\(sequenceNumber)"
-    
-        guard let data = payload.data(using: .utf8) else {
-            print("Error encoding data.")
+extension MultipeerManager : DSDVManagerLinkDelegate {
+    func launchSession(sessionName: String) -> Void {
+        guard createSession(sessionName: sessionName) != nil else {
+            print("Link Error: impossible to launch session. Session already exist.")
             return
         }
-        
-        sendBroadcastData(data: data)
+        inviteNeighborsToSession()
     }
     
-    func unicastApplicationPacket(nextHop: NodeID, destination: NodeID, applicationData: Data) {
-        let headerPayload = "1|\(destination)|"
-        
-        guard let headerData = headerPayload.data(using: .utf8) else {
-            print("Error encoding header data.")
+    func quitSession(sessionName: String) {
+        guard checkSessionName(sessionName: sessionName) else {
+            print("Link Error: impossible to leave session. Different session name provided.")
             return
         }
-        
-        sendUnicastData(
-            data: headerData + applicationData,
-            destination: nextHop
-        )
+        disconnectSession()
+    }
+    
+    func handleInvitationResponse(sessionName: String, accepted: Bool) {
+        guard let invitation = getInvitationHandler(sessionName: sessionName) else {
+            return
+        }
+        if accepted {
+            // accept to join session
+            guard let session = createSession(sessionName: sessionName) else {
+                print("Link Error: impossible to accept invitation, an error occur while creating session.")
+                removeInvitation(sessionName: sessionName)
+                return
+            }
+            invitation(accepted, session)
+            clearInvitations()
+            // propagate invitations to our neighbors
+            inviteNeighborsToSession()
+        } else {
+            // reject to join session
+            invitation(accepted, nil)
+            removeInvitation(sessionName: sessionName)
+        }
+    }
+    
+    func broadcastPacket(
+        data: Data
+    ) {
+        guard let peers = getSessionPeers() else {
+            return
+        }
+        guard !peers.isEmpty else {
+            return
+        }
+        sendData(peers: peers, data: data)
+    }
+    
+    func unicastPacket(
+        destination: NodeID, 
+        data: Data
+    ) {
+        guard let MCDestinationID = getMCIDfrom(displayName: destination) else {
+            return
+        }
+        sendData(peers: [MCDestinationID], data: data)
     }
 }
