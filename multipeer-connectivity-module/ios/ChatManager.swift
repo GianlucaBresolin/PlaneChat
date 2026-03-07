8ab7ded734b7135d3f40692ea0cc5ada9f8deb0b
Gianluca Bresolin <gianbreso02@gmail.com>
2026-03-07 22:00:37 +0100

    Add application layer (ChatManager).

---
+++ multipeer-connectivity-module/ios/ChatManager.swift
@@ -0,0 +1,71 @@
+import Foundation
+
+class ChatManager {
+    private let MyNodeID : NodeID
+    weak var NetworkDelegate : ChatManagerNetworkDelegate?
+    weak var PresentationDelegate : ChatManagerPresentationDelegate?
+
+    let IsolationQueue: DispatchQueue
+
+    init(
+        nodeID: NodeID
+    ) {
+        self.MyNodeID = nodeID
+        self.IsolationQueue = DispatchQueue(label: "ChatManagerIsolationQueue")
+    }
+
+    func createGroup(groupName: String) {
+        self.IsolationQueue.async {
+            guard let networkDelegate = self.NetworkDelegate else {
+                print("Application Error: impossible to create group, no network delegate available.")
+                return
+            }
+            networkDelegate.requestControl(
+                command: "createGroup",
+                groupName: groupName
+            )
+        }
+    }
+
+    func joinGroup(groupName: String) {
+        self.IsolationQueue.async {
+            guard let networkDelegate = self.NetworkDelegate else {
+                print("Application Error: impossibe to join group, no network delegate available.")
+                return
+            }
+            networkDelegate.requestControl(
+                command: "joinGroup",
+                groupName: groupName
+            )
+        }
+    }
+
+    func leaveGroup(groupName: String) {
+        self.IsolationQueue.async {
+            guard let networkDelegate = self.NetworkDelegate else {
+                print("Application Error: impossibe to leave group. No network delegate available.")
+                return
+            }
+            networkDelegate.requestControl(
+                command: "leaveGroup",
+                groupName: groupName
+            )
+        }
+    }
+
+    func sendMessage(
+        sender: String,
+        message: String
+    ) {
+        guard let networkDelegate = self.NetworkDelegate else {
+            print("Application Error: impossibe to send a message. No network delegate available.")
+            return
+        }
+        let applicationPacket = "\(sender)|\(message)"
+        guard let applicationPacketPayload = applicationPacket.data(using: .utf8) else {
+            print("Application Error: fail to encode application payload.")
+            return
+        }
+        networkDelegate.broadcastMessage(data: applicationPacketPayload)
+    }
+}
