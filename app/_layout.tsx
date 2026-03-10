3d53e2a76df7364d24413166abf79c6d957cee89
Gianluca Bresolin <gianbreso02@gmail.com>
2026-03-10 17:06:21 +0100

    Fix icon display error.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,25 @@
+import { Ionicons } from "@expo/vector-icons";
+import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
 
 export default function RootLayout() {
+
+  const [loaded, error] = useFonts({
+    ...Ionicons.font,
+  });
+
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded, error]);
+
+  if (!loaded && !error) {
+    return null;
+  }
+
   return <Stack 
     screenOptions = {{
       headerShown: false,
